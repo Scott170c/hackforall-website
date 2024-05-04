33134ca23ad07e58390a1e86d1c77e57497c6b2e
BRUHisbackbois <72882797+Scott170c@users.noreply.github.com>
2024-05-04 11:02:58 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,16 +10,24 @@ export default function Home() {
     <div>
       <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
       <main className={`${styles.main} ${aldrich.Aldrich}`}>
-
-        <h1><div className={styles.main_title}>
-          <img src="HACK.png" alt="HACK" className={`${styles.main_title_item} ${styles.hvr}`}></img>
-          <img src="4.png" alt="4" className={`${styles.main_title_item} ${styles.hvr}`}></img>
-          <img src="ALL.png" alt="ALL" className={`${styles.main_title_item} ${styles.hvr}`}></img>
-        </div></h1>
+        <div className={styles.center}>
+        <h1><b>
+          <div className={styles.main_text_div}>
+            <img src="HACK.png" alt="HACK" className={`${styles.hack} ${styles.hvr}`}></img>
+              <a className={styles.filler}>&nbsp;&nbsp;</a>&nbsp;
+              <img src="4.png" alt="4" className={`${styles.four} ${styles.hvr}`}></img>
+              <a className={styles.filler}>&nbsp;&nbsp;</a>&nbsp;
+            <img src="ALL.png" alt="ALL" className={`${styles.all} ${styles.hvr}`}></img>
+          </div>
+        </b></h1>
+        <br></br>
         <div className={styles.topscroll}>
+          Who We Are
           <div className={`${styles.arrow} ${styles.bounce}`}></div>
         </div>
-
+        </div>
+      </main>
+  
         <div className={styles.ourmission_container}>
           <img src="ourmission-wire.png" className={styles.ourmission_wire}></img>
           <img src="ourmission-wire-mobile.png" className={styles.ourmission_wire_mobile}></img>
