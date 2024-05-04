f1c3b6638fe8c0ba7633dcc563a68645d9ea5187
BRUHisbackbois <72882797+Scott170c@users.noreply.github.com>
2024-05-04 10:27:11 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,18 +7,13 @@ const lexend = Lexend({  weight: ['400'], subsets: ["latin"] });
 const Navbar = () => {
     return (
       <div className={`${styles.topnav} ${lexend.Lexend}`}>
-        {/* <a>
-          <img href="/" src="Hack4All-centered.png" alt="Home" className={styles.topnav_img}></img>
-        </a> */}
-        <a href="/"><img href="/" src="Hack4All Logo Blue-centered.png" alt="Home" className={styles.topnav_img}></img></a>
         <b>
-          <a href="https://hcb.hackclub.com/donations/start/hack4all" className={styles.topnav_item}>Donate</a>
-          <a href="/events" className={styles.topnav_item}>Events</a>
-          <a href="/contact" className={styles.topnav_item}>Contact Us</a>
+          <a href="https://hcb.hackclub.com/donations/start/hack4all" className={styles.topnav_text}>Donate</a>
+          <a href="/events" className={styles.topnav_text}>Events</a>
+          <a href="/contact" className={styles.topnav_text}>Contact Us</a>
         </b>
       </div>
       );
 };
-
 
 export default Navbar;
